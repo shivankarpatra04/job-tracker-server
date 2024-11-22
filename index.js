4bfbeadb4e9edb9b79390c72d5cadc7de186fe27
shivank04 <shivankar.rockit@gmail.com>
2024-11-22 13:14:51 +0530

    added frontend link

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin: ['https://job-tracker-mocha.vercel.app', 'http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
